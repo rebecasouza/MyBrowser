0ce492d47812abd210752ff07bab782af9905164
Rebeca Souza <rebeca@rebeca-2.local>
2017-05-15 20:14:43 -0400

    First commit

--- MyBrowser/ViewController.swift
+++ MyBrowser/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        view.addSubview(webView)
+        
+        //Overriding frame on webView
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let width = NSLayoutConstraint(item: webView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1.0, constant: 0)
+        let height = NSLayoutConstraint(item: webView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: -60)
+        let top = NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 60)
+        
+        view.addConstraints([width, height, top])
+    }
+   
+    @IBAction func goBack(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let url: URL = URL(string: textField.text!)!
+        let request: URLRequest = URLRequest(url: url)
+        textField.resignFirstResponder()
+        webView.load(request)
+        
+        return false
     }
 
     override func didReceiveMemoryWarning() {
